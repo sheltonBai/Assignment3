870cd1442005a31372fa9640418cee616b258871
S <s>
2017-07-12 12:25:35 -0400

    {"types":"C","runs":15}

--- MouseTracker.java
+++ MouseTracker.java
@@ -1,4 +1,18 @@
-
-public class MouseTracker {
-
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.event.*;
+public class MouseTracker extends GraphicsProgram {
+public void run() {
+label = new GLabel("");
+label.setFont("Times New Roman-36");
+add(label, 50, 50);
+// Must call this method to be able to get mouse events
+addMouseListeners();
+}
+// This method is called everytime user moves mouse
+public void mouseMoved(MouseEvent e) {
+label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")");
+}
+/* Private instance variable */
+private GLabel label;
 }
